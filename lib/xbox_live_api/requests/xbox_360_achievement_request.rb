9f809d2a6c788e16b6aa53817404f6368cc28c9a
Vladut Radulescu <pacMakaveli90@gmail.com>
2020-08-18 09:50:47 +0100

    fix(): fix 360 endpoint response

--- lib/xbox_live_api/requests/xbox_360_achievement_request.rb
+++ lib/xbox_live_api/requests/xbox_360_achievement_request.rb
@@ -1,6 +1,7 @@
 require 'xbox_live_api/requests/base_request'
 require 'xbox_live_api/requests/http_session_gateway'
 require 'xbox_live_api/json_parsers'
+require 'json'
 
 class XboxLiveApi
   module Requests
@@ -16,24 +17,26 @@ class XboxLiveApi
       private
 
       def get_unlocked_achievements(game, user_id)
-        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/achievements?titleId=#{game}"
+        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/achievements?titleId=#{game}&maxItems=250"
         get_achievements(url)
       end
 
       def get_all_achievements(game, user_id)
-        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/titleachievements?titleId=#{game}"
+        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/titleachievements?titleId=#{game}&maxItems=250"
         get_achievements(url)
       end
 
       def get_achievements(url)
-        json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_360)).body
-        @parser.parse_achievements_from(json)
+        json = HttpSessionGateway.new.get(url, header: header_for_version(3)).body
+        # json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_360)).body
+        return JSON.parse(json)
+        # @parser.parse_achievements_from(json)
       end
 
       def combine_achievements(all_achievements, unlocked_achievements)
-        unlocked_ids = unlocked_achievements.collect(&:id)
-        all_achievements.reject! { |achievement| unlocked_ids.include?(achievement.id) }
-        unlocked_achievements + all_achievements
+        unlocked_ids = unlocked_achievements['achievements'].collect { |a| a['id'] }
+        all_achievements['achievements'].reject! { |achievement| unlocked_ids.include?(achievement['id']) }
+        unlocked_achievements.merge(all_achievements)
       end
     end
   end
--- lib/xbox_live_api/requests/xbox_one_achievement_request.rb
+++ lib/xbox_live_api/requests/xbox_one_achievement_request.rb
@@ -1,15 +1,18 @@
 require 'xbox_live_api/requests/base_request'
 require 'xbox_live_api/requests/http_session_gateway'
 require 'xbox_live_api/json_parsers'
+require 'json'
 
 class XboxLiveApi
   module Requests
     class XboxOneAchievementRequest < BaseRequest
 
       def for(user_id, game)
-        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/achievements?titleId=#{game}"
-        json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_ONE)).body
-        JsonParsers::XboxOneAchievementJsonParser.new.parse_achievements_from(json)
+        url = "https://achievements.xboxlive.com/users/xuid(#{user_id})/achievements?titleId=#{game}&maxItems=250"
+        json = HttpSessionGateway.new.get(url, header: header_for_version(4)).body
+        # json = HttpSessionGateway.new.get(url, header: header_for_version(Version::XBOX_ONE)).body
+        return JSON.parse(json)
+        # JsonParsers::XboxOneAchievementJsonParser.new.parse_achievements_from(json)
       end
     end
   end

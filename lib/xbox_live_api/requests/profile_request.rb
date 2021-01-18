03bbbe20ae6584caa27fc3b34c8c598979daef7d
Vlad <pacMakaveli90@gmail.com>
2021-01-18 12:08:38 +0000

    fix(): fix the profile request endpoint

--- lib/xbox_live_api/requests/profile_request.rb
+++ lib/xbox_live_api/requests/profile_request.rb
@@ -1,44 +1,61 @@
 require 'xbox_live_api/requests/base_request'
-require 'json'
+require 'xbox_live_api/profile'
+require 'oj'
 
 class XboxLiveApi
   module Requests
     class ProfileRequest < BaseRequest
-      PROFILE_ENDPOINT ||= 'https://profile.xboxlive.com/users/batch/profile/settings'
+      PROFILE_V1_ENDPOINT ||= 'https://profile.xboxlive.com/users/batch/profile/settings'
+      PROFILE_V2_ENDPOINT ||= 'https://peoplehub.xboxlive.com/users/%s/batch/profile/settings'
 
       def for(user_id)
-        params = {
-          'settings' => %w(Gamerscore Gamertag GameDisplayPicRaw AccountTier XboxOneRep PreferredColor TenureLevel Motto Bio Location),
-          'userIds' => [user_id]
-        }
-
-        response = HttpSessionGateway.new
-          .post_json(PROFILE_ENDPOINT,
-            header: header_for_version(Version::XBOX_ONE),
-            body: params
-          )
-
-        response = JSON.parse(response.body)
-
-        identity = {
-          'id' => response.dig('profileUsers', 0, 'id'),
-          'isSponsoredUser' => response.dig('profileUsers', 0, 'isSponsoredUser')
-        }.merge(
-          pretty_key_value(response.dig('profileUsers', 0, 'settings'))
-        )
-
-        return identity
+        resp = make_request(user_id)
+        handle_response(resp, user_id)
       end
 
     private
 
-      def pretty_key_value(settings)
-        settings_hash = {}
+      def make_request(user_id)
+        params = {
+          'settings' => %w(
+            AccountTier AppDisplayName AppDisplayPicRaw
+            Bio
+            IsQuarantined
+            Location
+            ModernGamertag ModernGamertagSuffix
+            GameDisplayName GameDisplayPicRaw Gamerscore Gamertag
+            Motto
+            PreferredColor PublicGamerpic
+            RealName RealNameOverride
+            ShowUserAsAvatar
+            TenureLevel
+            UniqueModernGamertag
+            XboxOneRep
+            Watermarks
+          ),
+          'userIds' => [user_id]
+        }
 
+        HttpSessionGateway.new
+         .post_json(PROFILE_V1_ENDPOINT,
+           header: header_for_version(Version::XBOX_ONE),
+           body: params
+         ).body
+      end
+
+      def handle_response(resp, user_id)
+        json = Oj.load(resp)
+        settings = json['profileUsers'].first['settings']
+        settings_hash = collect_settings(settings)
+
+        return json
+      end
+
+      def collect_settings(settings)
+        settings_hash = {}
         settings.each do |setting|
           settings_hash.store(setting['id'], setting['value'])
         end
-
         settings_hash
       end
     end
